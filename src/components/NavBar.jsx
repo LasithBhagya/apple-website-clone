7aee99b3f40f52f02af01ac0759bae426733cfa1
Lasith Bhagya <lasith13@gmail.com>
2026-01-30 21:07:20 +0530

    Implement hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { navLinks } from '../constants'
 
 const NavBar = () => {
-  return (
-    <header>
-        <nav>
-            <img src='logo.svg' alt='Apple logo' />
+    return (
+        <header>
+            <nav>
+                <img src='logo.svg' alt='Apple logo' />
 
-            <ul>
-                {navLinks.map((link) => (
-                    <li key={link.label}> <a href={link.label}>{link.label}</a> </li>
-                ))}
-            </ul>
+                <ul>
+                    {navLinks.map(({label}) => (
+                        <li key={label}> <a href={label}>{label}</a> </li>
+                    ))}
+                </ul>
 
-            <div className="flex-center gap-3">
-                <button> <img src="/search.svg" alt="Search" /> </button>
-                <button> <img src="/cart.svg" alt="Cart" /> </button>
-            </div>
-        </nav>
-    </header>
-  )
+                <div className="flex-center gap-3">
+                    <button> <img src="/search.svg" alt="Search" /> </button>
+                    <button> <img src="/cart.svg" alt="Cart" /> </button>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default NavBar
